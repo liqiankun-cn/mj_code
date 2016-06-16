f7171d581d52751419c9af450b78624cb26629c9
mj.li <sky_007_008@sina.com>
2016-06-16 19:56:17 +0800

    测试多态

---
+++ src/testClass/TestClass.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package testClass;
+
+/**
+ * @author qiankun.li
+ *
+ */
+public class TestClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Wine a = new Jnc();
+		a.fun1();
+		a.setI(2);
+		System.out.println(a.i);
+		System.out.println(a.getI());
+		
+	}
+
+}
