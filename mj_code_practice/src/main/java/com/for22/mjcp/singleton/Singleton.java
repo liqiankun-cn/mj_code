ea272e66ab67c531a4ca7720dfbf7dc1210a7c4a
liqiankun <liqiankun@192.168.100.107>
2017-05-29 12:38:30 +0800

    删除

---
+++ mj_code_practice/src/main/java/com/for22/mjcp/singleton/Singleton.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.for22.mjcp.singleton;
+
+/**
+ * @author mj
+ *
+ */
+public class Singleton {
+
+	public static final Singleton SINGLETON = new Singleton();
+
+	public static void main(String[] args) {
+		new String().getBytes();
+		Object obj = new Object();
+	}
+
+}
