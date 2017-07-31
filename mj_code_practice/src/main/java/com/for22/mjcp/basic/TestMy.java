07cc3e86d9c1009934a974391ace9a4e67a7bacc
liqiankun <liqiankun@zhongan.com>
2017-07-31 12:39:19 +0800

    提交测试类

--- mj_code_practice/src/main/java/com/for22/mjcp/basic/TestMy.java
+++ mj_code_practice/src/main/java/com/for22/mjcp/basic/TestMy.java
@@ -11,16 +11,25 @@ import com.for22.mjcp.basic.Outter.Inner;
  */
 public class TestMy {
 
+	static int num = 0; 
 	/**
 	 * @param args
 	 */
+	@SuppressWarnings("null")
 	public static void main(String[] args) {
 
-		Outter o = new Outter();
 		Outter.Inner inner = new  Outter.Inner();
 		Inner inner2 = new Inner();
+		Outter o = new Outter();
+		int num3 = 3;
 		System.out.println(inner.test("xx"));
+		if(inner2 != null)
+			o =null;
+		num = 2;
+		System.out.println("test J!"+o.hashCode());
 		System.out.println(inner2.test("oo"));
+		
+		System.out.println("test condition");
 		
 	}
 
