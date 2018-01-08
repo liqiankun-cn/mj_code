e98f7eaad7057b954ae4670d15a1a640bd25bcce
lixing <lixing@houbank.com>
2018-01-08 09:59:47 +0800

    【算法题】最长子串

---
+++ mj_code_practice/src/main/java/com/for22/mjcp/algorithm/LongSubString.java
@@ -0,0 +1,34 @@
+package com.for22.mjcp.algorithm;
+
+/**
+ * The Type LongSubString
+ *
+ * 给出两个字符串，写一个函数找到最长公共子串，并返回其长度。
+
+ 注意事项：
+ 子串的字符应该连续的出现在原字符串中，这与子序列有所不同。
+
+ 格式：
+
+ 输入行输入两个字符串 A 和 B，最后输出最长公共子串的长度。
+
+ 样例输入
+
+ A = “ABCD”
+ B = “CBCE”
+
+ 样例输出
+
+ 2
+
+ *
+ * @author lx
+ * @Description:
+ * @Date 2017/12/8
+ */
+public class LongSubString {
+
+
+
+}
+ 
